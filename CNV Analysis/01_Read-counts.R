b3e61f3eb7c2b35c8b0fdce864153eb1080e1e21
Drew Thompson <drewthompson@login3.ufhpc>
2021-08-02 20:22:35 -0400

    Calculate read counts

--- CNV Analysis/01_Read-counts.R
+++ CNV Analysis/01_Read-counts.R
@@ -1,20 +1,33 @@
 library(cn.mops)
-samples <- list.files(path = "/orange/cschnitzler/NS1865-MMartindale_HFHKVDSXY-Lane1-4_DNASeq2020Data/NS1865-MMartindale_HFHKVDSXY-Lane1-4/merged_reads/Platypus_input_files/", pattern = "\\.bam$", full.names = TRUE)
-reference.scaffolds <- "/orange/cschnitzler/NS1865-MMartindale_HFHKVDSXY-Lane1-4/NS1865-MMartindale_HFHKVDSXY-Lane1-4/merged_reads/chelonia_mydas_new_referencescaffolds.txt"
-output.path <- "/blue/cschnitzler/drewthompson/turtle-fp-wgs/readcounts/"
+library(tidyverse) # for string split
+
+#### 0. Prepare Reference Scaffold ####
+reference.scaffolds <- "/orange/cschnitzler/NS1865-MMartindale_HFHKVDSXY-Lane1-4_DNASeq2020Data/NS1865-MMartindale_HFHKVDSXY-Lane1-4/merged_reads/chelonia_mydas_new_referencescaffolds.txt" 
 reference.scaffolds <- read.table(reference.scaffolds, header = T, check.names = F, sep = "\t")
 reference.scaffolds <- reference.scaffolds[-c(29:98), ]
 reference.scaffolds <- as.character(reference.scaffolds[, "RefSeq-Accn"])
+
+#### 1. Indicate Setttings ####
+samples <- list.files(path = "/orange/cschnitzler/NS1865-MMartindale_HFHKVDSXY-Lane1-4_DNASeq2020Data/NS1865-MMartindale_HFHKVDSXY-Lane1-4/merged_reads/Platypus_input_files/", pattern = "\\.bam$", full.names = TRUE)
+windowsize <- 1000
+output.path <- "/blue/cschnitzler/drewthompson/turtle-fp-wgs/CNV Analysis/readcounts/"
 cat("\n Counting reads in specified tumour and host .bam files... \n")
-windowsize <- 5000
 for (sample in samples) {
   cat(paste("\n", sample, "\n"))
+  tumor <- str_split_fixed(str_split_fixed(sample, "/", 8)[, 8], "_", 4)[, 2]
+count_file <-  paste0(output.path, "readcounts_", windowsize, "BP_", tumor, ".txt")
+if(file.exists(count_file)){
+cat("\n Read count file already exists, skipping. \n")
+}
+else{
+#### 2. Call Read Counts with cn.mops ####
   readcounts <- getReadCountsFromBAM(
     BAMFiles = sample,
     sampleNames = paste(sample),
     WL = windowsize,
     refSeqName = reference.scaffolds
   )
+#### 3. Format Read Counts ####
   cat("\n Formatting read count outputs... \n")
   counts.df <- data.frame(
     seqnames = seqnames(readcounts),
@@ -24,11 +37,16 @@ for (sample in samples) {
   )
   counts.mt <- as.matrix(counts.df)
   colnames(counts.mt)[1:4] <- c("SCAFFOLD", "START", "END", "SAMPLE COUNTS")
-  cat("\n Writing read count matrix to output... \n")
-  tumor <- str_split_fixed(str_split_fixed(sample, "/", 8)[, 8], "_", 4)[, 2]
+#### 4. Save Read Counts ####  
+cat("\n Writing read count matrix to output... \n")
+  write.table(counts.mt, # backup in case the next write errors 
+    file = "temp.txt",
+    quote = F, col.names = TRUE, row.names = F, sep = "\t"
+  )
   write.table(counts.mt,
-    file = paste0(output.path, "readcounts_", windowsize, "BP_", sample, ".txt"),
+    file = count_file,
     quote = F, col.names = TRUE, row.names = F, sep = "\t"
   )
   # save(counts.mt, file = paste0(output.path, "readcounts_", windowsize, "BP_yuRKTW1Fdna.Rdata"))
+}
 }
--- CNV Analysis/01_Read-counts.sh
+++ CNV Analysis/01_Read-counts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=read_counts
 #SBATCH --nodes=1
-#SBATCH --mem=10gb
-#SBATCH --time=6:00:00
+#SBATCH --mem=16gb
+#SBATCH --time=2:00:00
 #SBATCH --account=cschnitzler
 #SBATCH --qos=cschnitzler-b
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
@@ -10,6 +10,6 @@
 #SBATCH --output=read_counts-%j.log
 pwd;hostname;date
 module load R
-Rscript read_counts2.R
+Rscript 01_Read-counts.R
 date
 
