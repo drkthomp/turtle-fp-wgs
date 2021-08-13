558b685a0408f55b35ece3d56caadd932adaffdc
drkthomp <drkthomp@ucsc.edu>
2021-08-13 18:28:56 -0400

    faster repetition and distribution plots

--- CNV Analysis/02_CNV-Calling.R
+++ CNV Analysis/02_CNV-Calling.R
@@ -3,6 +3,7 @@ library(GenomicRanges)
 library(tidyverse)
 library(janitor)
 library(GenomicFeatures)
+library(BSgenome.Cmydas.NCBI.rCheMyd1)
 
 # Calls CNVs and performs basic filtering of the read counts
 
@@ -14,7 +15,9 @@ dataloc <- "readcounts/" # note where the readcounts are
 # and the final file name before the tumor:
 file_prefix <- paste0(dataloc, "readcounts_", bp, "BP_")
 sample_data <- read.csv("partials/sample_data.tsv", sep = "")
+protein_table <- makeGRangesFromDataFrame(read.delim("~/2021-REU/CNV Analysis/ProteinTable_13308_1483792_Cm_NEW_NCBI.txt", quote = "") %>% clean_names() %>% dplyr::rename("seqnames" = "accession") %>% dplyr::select(-x_name), keep.extra.columns = TRUE)
 
+annoloc <- "02-Annotation/"
 
 #### 2. Load Reference Data ####
 txdb <- loadDb("rCheMyd1.sqlite") # generated from rCheMyd1
@@ -41,6 +44,11 @@ if (file.exists(ref_seq_file)) {
   seqnames(ref_seq) <- ref$RefSeqAccn
   save(ref_seq, file = ref_seq_file)
 }
+
+dna <- extractTranscriptSeqs(BSgenome.Cmydas.NCBI.rCheMyd1, txdb,
+                             use.names = TRUE
+)
+aa <- suppressWarnings(translate(dna))
 
 #### 3. Define Functions ####
 ##### 3.1 Count Functions #####
@@ -144,7 +152,7 @@ filterCountsAcrossSamples <- function(norm_grange, filter = 0.01,
     })
     if (is.null(norm_data)) { # exit early in case of complete null
       return(NULL)
-    } #otherwise continue
+    } # otherwise continue
     norm_data <- norm_data %>% purrr::reduce(cbind)
     norm_data <- norm_data[, !duplicated(colnames(norm_data))] %>%
       rename(chr = value)
@@ -209,16 +217,26 @@ prepCounts <- function(sample_data, bp = 5000, normType = "poisson",
 
 ##### 3.2 Segmentation Function #####
 ###### 3.2.1 Calling Segmentation ######
-callCNVfromNorm <- function(norm, minReadCount = 5, parallel = 2) {
-  segmented <- referencecn.mops(
-    cases = norm[, "TUMOUR"],
-    controls = norm[, "HOST"],
-    minReadCount = minReadCount,
-    segAlgorithm = "fast",
-    norm = 0, # since we're inputting already normalized read counts
-    parallel = parallel
-  )
-  segmented <- calcIntegerCopyNumbers(segmented)
+callCNVfromNorm <- function(name, norm_full, minReadCount = 5, parallel = 2) {
+  message("Starting segmentation call for ", name)
+  file <- paste0("partials/segmented/temp/", minReadCount, "-", name, ".gz")
+  if (file.exists(file)) {
+    message("Temporary seg call exists at ", file)
+    load(file)
+  } else {
+    norm <- norm_full[[name]]
+    segmented <- referencecn.mops(
+      cases = norm[, "TUMOUR"],
+      controls = norm[, "HOST"],
+      minReadCount = minReadCount,
+      segAlgorithm = "fast",
+      norm = 0, # since we're inputting already normalized read counts
+      parallel = parallel
+    )
+    segmented <- calcIntegerCopyNumbers(segmented)
+    message("Saved temporary file at ", file)
+    save(segmented, file = file)
+  }
   return(segmented)
 }
 ###### 3.2.2 Filtering ######
@@ -293,63 +311,253 @@ filterCNVTumour <- function(norm, cnvs, filter = 0.05) {
 
 
 ###### 3.2.3 Visualization ######
+plotClear <- function(){
+  if((!is.null(dev.list())) & length(dev.list()) > 0){
+    for (i in dev.list()[1]:dev.list()[length(dev.list())]) {
+      tryCatch(dev.off(),error=message)
+    }}
+}
+plotSingleCNVs <- function(which, tum, segmented_single, base_loc, margin = 10) {
+  tryCatch({
+  file <- paste0(base_loc, paste(tum, cnvr(segmented_single)[which], mcols(cnvr(segmented_single))[which, ], sep = "-"), ".png")
+  },error=function(cond){
+    message(paste("Failed plotting CNV", which, tum))
+    message(cond)
+    return(NA) # exit before trying to start plotting
+  })
+  message(paste("Plotting single CNV", which,"at", file))
+  png(filename = file, width = 1000, height = 965)
+  tryCatch(plot(segmented_single, which = which, margin = c(margin, margin)),error=print)
+  dev.off()
+  plotClear()
+
+}
+
 plotCNV <- function(tum, segmented, base_loc, lim = 5) {
-  chromplot_file <- paste0(base_loc, "-", tum, " FP - chromosome segplot.png")
-  segplot_file <- paste0(base_loc, "-", tum, " FP - segplot.pdf")
+  chromplot_file <- paste0(base_loc, tum, " FP - chromosome segplot.png")
+  segplot_file <- paste0(base_loc, tum, " FP - segplot.pdf")
+  size_file <- paste0(base_loc, tum, " FP - Size Distribution.png")
+  type_file <- paste0(base_loc, tum, " FP - Type Distribution.png")
   if (!(reuse && file.exists(chromplot_file))) {
     message("Generating chromosome plot at ", chromplot_file)
-    try({
-      png(
-        file = chromplot_file,
-        width = 3000,
-        height = 1500
-      )
+
+    png(
+      filename = chromplot_file,
+      width = 3000,
+      height = 1500
+    )
+    tryCatch(
       segplot(
         segmented[[tum]],
         ylim = c(-lim, lim),
         plot.type = "s"
-      )
-      dev.off()
-    })
+      ),error=message
+    )
+    dev.off()
+    plotClear()
   }
   if (!(reuse && file.exists(segplot_file))) {
     message("Generating chromosome plot at ", segplot_file)
-    try({
-      pdf(segplot_file,
-        width = 18,
-        height
-        = 9
-      )
+
+    pdf(file=segplot_file,
+      width = 18,
+      height
+      = 9
+    )
+    tryCatch(
       segplot(
         segmented[[tum]],
         ylim = c(-lim, lim),
         plot.type = "w",
         pt.cols = c("#333333", "#000000")
-      )
-      dev.off()
-    })
+      ),error=message
+    )
+    dev.off()
+    plotClear()
   }
+  data <- data.frame(width=width(cnvr(segmented[[tum]])),type=mcols(cnvr(segmented[[tum]]))$TUMOUR)
+  if(!(reuse && file.exists(size_file))){
+    ggplot(data, aes(x=width,fill=type)) + geom_histogram(bins=100) + labs(title=paste(tum, "Width Distribution"),x="Width of CNV Region") + theme_minimal()
+    ggsave(filename = size_file)
+  }
+  if(!(reuse && file.exists(type_file))){
+    ggplot(data, aes(x=parse_number(type))) + geom_histogram(stat="count",binwidth=1) + labs(title=paste(tum, "Type Distribution"),x="CNV Type") + theme_minimal()
+    ggsave(filename = type_file)
+  }
+
+  # just in case
+  plotClear()
+ # if(length(cnvr(segmented[[tum]])) > 0){
+ #   lapply(1:length(cnvr(segmented[[tum]])), plotSingleCNVs, tum = tum, segmented = segmented[[tum]], base_loc = base_loc)
+ # }
+ # plotClear()
 }
 
 
+##### 3.3 Annotation Functions #####
+
+# The functions to annotate both for transcripts and genes:
+saveSeq <- function(cnv, seqs, save_file) {
+  # get the actual protein sequences
+  sequences <- getSeq(seqs, as.character(unlist(cnv$tx_name)))
+
+  # and save them for lookup
+  writeLines(paste0("> ", names(sequences), "\n", sequences), save_file)
+}
+
+annotateAll <- function(cnv, protein_table, type = "any") {
+  # Modifed from overlap by Martin Morgan
+  # from https://support.bioconductor.org/p/96427/
+  stopifnot(is(cnv, "GRanges"), is(protein_table, "GRanges")) # required for find overlaps
+  olaps <- GenomicRanges::findOverlaps(cnv, protein_table, ignore.strand = TRUE, type = type)
+  mcols(olaps) <- as.data.frame(protein_table[subjectHits(olaps)]) %>% mutate(protein_name_short = str_squish(str_replace_all(str_remove_all(protein_name, '-like|isoform|LOW QUALITY PROTEIN|"'), "F10", "F")))
+  names(mcols(olaps))[1:5] <- paste0("gene_", names(mcols(olaps))[1:5])
+  cnv_factor <- factor(queryHits(olaps), levels = seq_len(queryLength(olaps)))
+  mcols(cnv) <- cbind(mcols(cnv), apply(mcols(olaps), MARGIN = 2, function(x) {
+    splitAsList(x, cnv_factor)
+  }))
+  return(cnv)
+}
+
+annotateTranscripts <- function(cnv, txdb, tum, type = "any") {
+  # by Martin Morgan
+  # from https://support.bioconductor.org/p/96427/
+  stopifnot(is(cnv, "GRanges"), is(txdb, "TxDb")) # error checking
+  anno <- transcripts(txdb) # get the genes from the ref
+  olaps <- findOverlaps(cnv, anno, ignore.strand = TRUE, type = type) # find the overlaps
+  mcols(olaps)$tx_name <- anno$tx_name[subjectHits(olaps)]
+  cnv_factor <- factor(queryHits(olaps), levels = seq_len(queryLength(olaps)))
+  cnv$tx_name <- splitAsList(mcols(olaps)$tx_name, cnv_factor) # and add them to the cnvs
+  return(cnv)
+}
+
+annotateGenes <- function(cnv, txdb) {
+  # by Martin Morgan
+  # from https://support.bioconductor.org/p/96427/
+  stopifnot(is(cnv, "GRanges"), is(txdb, "TxDb")) # error checking
+  anno <- genes(txdb) # get the genes from the ref
+  olaps <- findOverlaps(cnv, anno, ignore.strand = TRUE, type = type) # find the overlaps
+  mcols(olaps)$gene_id <- anno$gene_id[subjectHits(olaps)]
+  cnv_factor <- factor(queryHits(olaps), levels = seq_len(queryLength(olaps)))
+  cnv$gene_id <- splitAsList(mcols(olaps)$gene_id, cnv_factor) # and add them to the cnvs
+  return(cnv)
+}
+
+throwout <- "yu"
+throwout_percent <- .05
+
+toThrowout <- function(start) {
+  loc <- as.numeric(start) / as.numeric(ref[ref$RefSeqAccn == "NC_051241.1", ]$SequenceLength)
+  return(loc < throwout_percent | loc > (1 - throwout_percent))
+}
+
+
+cnvDownstream <- function(tum, src, segmented, type = "any") {
+  # bp <- str_split_fixed(x, "/",3)[1]
+  # normType <-  str_split_fixed(x, "/",3)[2]
+  # sizeFactor <-  str_split_fixed(str_split_fixed(x, "/",3)[3], "-",4)[1]
+  # minReadCount <- str_split_fixed(str_split_fixed(x, "/",3)[3], "-",4)[2]
+
+
+  for (loc in c(annoloc, "partials/cnv")) {
+    # ensure that the directories are created
+    dir.create(paste(loc, type, sep = "/"))
+    dir.create(paste(loc, type, str_split_fixed(src, "/", 3)[1], sep = "/"))
+    dir.create(paste(loc, type, str_split_fixed(src, "/", 3)[1], str_split_fixed(src, "/", 3)[2], sep = "/"))
+  }
+
+  # define where everything will be saved
+  files <- list(
+    cnv = paste0("partials/cnv/", type, "/", str_remove(src, "\\.gz"), "-", tum, "_cnvs.gz"),
+    raw_protein = paste0(annoloc, type, "/", str_remove(src, ".gz"), "-", tum, "_proteins.txt"),
+    gene  =  paste0(annoloc, type, "/", str_remove(src, ".gz"), "-", tum, "_cnvs.txt"),
+    table = paste0(annoloc, type, "/", str_remove(src, ".gz"), "-", tum, "_cnvs.tsv"),
+    protein = paste0(annoloc, type, "/", str_remove(src, ".gz"), "-", tum, "_protein_sequences.txt"),
+    dna = paste0(annoloc, type, "/", str_remove(src, ".gz"), "-", tum, "_dna_sequences.txt"))
+
+
+  if (reuse & all(lapply(files, file.exists) %>% unlist())) {
+    message("All files exist for ", tum) # don't redo anything if everything exists, just move on
+  } else {
+    if (reuse & file.exists(files$cnv)) {
+      message("Loading annotate cnv file ", files$cnv)
+      load(files$cnv)
+    } else {
+      print(paste0("=== ANNOTATING FOR TUMOR ", tum))
+      # add annotations
+
+      cnv <- cnvr(segmented[[tum]]) # should be loaded in
+      # cnv <- annotateGenes(cnv, txdb)
+      # cnv <- annotateTranscripts(cnv, txdb, tum)
+      cnv <- annotateAll(cnv, protein_table, type = type)
+
+      # and then save the annotations
+      message("Saving annotation file ", files$cnv)
+      save(cnv, file = files$cnv, compress = TRUE)
+    }
+
+
+    cnv <- as.data.frame(cnv) %>% mutate(throwout = toThrowout(start) | toThrowout(end))
+    if (grepl(throwout, tum)) {
+      print(paste("Threw out", count(cnv %>% filter(throwout)), "regions"))
+      print(cnv %>% filter(throwout))
+      cnv <- cnv %>% filter(!throwout)
+    }
+
+    if (!(reuse & file.exists(files$gene))) {
+      message("saving gene file at ", files$gene)
+      write(cnv$locus %>% unlist(), files$gene) # only gene information
+    }
+    if (!(reuse & file.exists(files$raw_protein))) {
+      message("saving raw proteins names for STRINGDB at ", files$raw_protein)
+      write(cnv$protein_name_short %>% unlist(), files$raw_protein) # only gene information
+    }
+    if (!(reuse & file.exists(files$table))) {
+      message("saving cnv table at ", files$table)
+      write.table( # more full data, allows recreation
+        cnv,
+        file = files$table,
+        quote = FALSE,
+        sep = "\t",
+        row.names = FALSE
+      )
+    }
+    # and then save the protein and dna sequences based on the annotated genes
+    if (!(reuse & file.exists(files$protein))) {
+      message("Saving cnv protein sequences at ", files$protein)
+      saveSeq(cnv, aa, files$protein)
+    }
+    if (!(reuse & file.exists(files$dna))) {
+      message("Saving cnv dna sequences at ", files$dna)
+      saveSeq(cnv, dna, files$dna)
+    }
+  }
+
+}
 #### 4. Run Pipleline ####
 partloc <- paste0("partials/segmented/", bp, "/")
-plotloc <- paste0("01_CNV-Plots/", bp, "/")
+plotloc <- paste0("01_CNV-Plots/", bp)
 
-dir.create(partloc)
-dir.create(plotloc)
+dir.create(partloc, showWarnings = FALSE)
+dir.create(plotloc, showWarnings = FALSE)
 
 # for (bp in c(5000, 1000)) {
 # here the point is to run multiple norm options
 for (normType in c("poisson", "mode", "mean", "median")) {
-  dir.create(paste0("partials/segmented/", bp, "/", normType))
-  dir.create(paste0("partials/normalized/", bp, "/", normType))
+  dir.create(paste0("partials/segmented", bp, normType, sep = "/"), showWarnings = FALSE)
+  dir.create(paste0("partials/normalized", bp, normType, sep = "/"), showWarnings = FALSE)
+  dir.create(paste(plotloc, normType, sep = "/"), showWarnings = FALSE)
 
   # with multiple possible size factors
   for (sizeFactor in c("mean", "median", "mode")) {
+    dir.create(paste0("partials/segmented", bp, normType, sizeFactor, sep = "/"), showWarnings = FALSE)
+    dir.create(paste0("partials/normalized", bp, normType, sizeFactor, sep = "/"), showWarnings = FALSE)
+    dir.create(paste(plotloc, normType, sizeFactor, sep = "/"), showWarnings = FALSE)
     for (filter in c(0, 0.01, 0.05)) { # and different filters for across samples
+      dir.create(paste(plotloc,normType,  sizeFactor, filter, sep = "/"), showWarnings = FALSE)
       for (byChrom in c(TRUE, FALSE)) { # either by chrom or not by chrom
         # then we can normalize and save each option
+        dir.create(paste(plotloc, normType, sizeFactor, filter, byChrom, sep = "/"), showWarnings = FALSE)
         norm_grange <- prepCounts(sample_data,
           bp = bp, normType = normType,
           sizeFactor = sizeFactor, filter = filter,
@@ -357,31 +565,54 @@ for (normType in c("poisson", "mode", "mean", "median")) {
         )
         # then the only seg option, minReadCount
         for (minReadCount in c(4, 8, 10, 16, 20, 32)) {
+          dir.create(paste(plotloc, normType, sizeFactor, filter, byChrom, minReadCount, sep = "/"), showWarnings = FALSE)
+          partial_footer <- paste0(bp, "/", normType, "/",
+          sizeFactor, "-", filter, "-", byChrom, "-", minReadCount, ".gz")
           seg_file <- paste0(
-            "partials/segmented/", bp, "/", normType, "/",
-            sizeFactor, "-", filter, "-", byChrom, "-", minReadCount, ".gz"
+            "partials/segmented/",partial_footer
           )
           if (reuse & file.exists(seg_file)) {
-            message("Segmentation file exists for minReadCount ", minReadCount)
-            load(seg_file)
+            message("Complete segmentation file exists for minReadCount ", minReadCount, ".")
           } else {
             message("Running segmentation for minReadCount ", minReadCount)
-            segmented <- lapply(norm_grange, callCNVfromNorm, minReadCount = minReadCount)
+            dir.create("partials/segmented/temp")
+            segmented <- lapply(names(norm_grange), callCNVfromNorm, norm_full = norm_grange, minReadCount = minReadCount)
+            unlink("partials/segmented/temp/", recursive = TRUE)
+            names(segmented) <- names(norm_grange)
             save(segmented, file = seg_file)
           }
+          # ensure directories exist
 
-          message("Plotting segmentation")
+
+          tum <- names(norm_grange)[length(norm_grange)]
+        base_loc <- paste(plotloc, normType, sizeFactor, filter, byChrom, minReadCount, "", sep = "/")
+          seg_output_files <-  list(chromplot = paste0(base_loc, tum, " FP - chromosome segplot.png"),
+                                    segplot = paste0(base_loc, tum, " FP - segplot.pdf"),
+                                    size = paste0(base_loc, tum, " FP - Size Distribution.png"),
+                                    type = paste0(base_loc, tum, " FP - Type Distribution.png"),
+                                    dna = paste0(annoloc, "any", "/", str_remove(partial_footer, ".gz"), "-", names(norm_grange)[length(names(norm_grange))], "_dna_sequences.txt"))
+
+          if(reuse & all(lapply(seg_output_files, file.exists) %>% unlist())){
+            message("All output files exists, skipping annotation and plotting.")
+          } else {
+            message("Loading segmentation")
+            
+
+            load(seg_file)
+            message("Plotting segmentation")
           lapply(names(segmented), plotCNV,
             segmented = segmented,
-            base_loc = paste0(
-              plotloc, "/", normType, "/",
-              sizeFactor, "-", filter, "-",
-              byChrom, "-", minReadCount
-            )
+            base_loc = base_loc
           )
-          rm(segmented)
-          gc()
+          message(paste("Starting annotation for within on", paste(names(segmented), collapse = " ")))
+          lapply(names(segmented), cnvDownstream, src = partial_footer, segmented = segmented, type = "within")
+          message(paste("Starting annotation for any on", paste(names(segmented), collapse = " ")))
+          lapply(names(segmented), cnvDownstream, src = partial_footer, segmented = segmented, type = "any")
+          }
         }
+
+
+        gc()
       }
     }
   }
